bd2baacb66a93b592bee15b5184729daffe8bb07
Haykal <moragasfarage.haykal@gmail.com>
2025-07-30 16:03:13 -0300

    Scene Transition
    - Adiciona pastas
    - Cria prefab de Loader de nível
    - Cria scenes de teste
    - Cria animation de Loader de nível

---
+++ GMTK-2025/Assets/Scripts/Transition/LevelLoader.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    [Header("Link in Inspector")]
+    public Animator transition;
+
+
+    [Header("Transition Parameters")]
+    public float transitionTime = 1f;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
