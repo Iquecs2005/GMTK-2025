c60e6fa8f092f7a18d2d68a55fd24166feb66f36
Haykal <moragasfarage.haykal@gmail.com>
2025-08-01 17:54:17 -0300

    Att liberação de níveis
    faz outras coisas também, mas não lembro rs

--- GMTK-2025/Assets/Scripts/Objects/LixoController.cs
+++ GMTK-2025/Assets/Scripts/Objects/LixoController.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class LixoController : MonoBehaviour
 {
     [Header("Components")]
     [SerializeField] private TMP_Text lixoCounter;
-    
+
     [Header("Events")]
     [SerializeField] private UnityEvent OnWin;
-    
+
     private int lixoInScene = 0;
     private int lixoCollected = 0;
     private bool hasWon;
 
-    public void AddLixoScene(int amount) 
+    public void AddLixoScene(int amount)
     {
         lixoInScene += amount;
         UpdateLixoCounter();
@@ -29,17 +30,28 @@ public class LixoController : MonoBehaviour
         lixoCollected += amount;
         UpdateLixoCounter();
 
-        if (lixoCollected >= lixoInScene) 
+        if (lixoCollected >= lixoInScene)
         {
             OnWin.Invoke();
             hasWon = true;
+            UnlockNewLevel();
             Time.timeScale = 0;
         }
     }
 
-    private void UpdateLixoCounter() 
+    private void UpdateLixoCounter()
     {
         string displayText = $"{lixoCollected}/{lixoInScene}";
         lixoCounter.text = displayText;
+    }
+
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
     }
 }
